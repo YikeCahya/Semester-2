bc0f7ba0366fdb585db06cf37050eee357aadb48
YikeCahya <yikecahya@gmail.com>
2025-05-27 09:08:22 +0700

    CASE METHOD 2

---
+++ Minggu14/BBM.java
@@ -0,0 +1,10 @@
+//Class untuk menyimpan jenis BBM dan harga per liter
+public class BBM {
+    String namaBBM;
+    double hargaPerLiter;
+
+    public BBM(String namaBBM, double harga) {
+        this.namaBBM = namaBBM;
+        this.hargaPerLiter = harga;
+    }
+}
---
+++ Minggu14/Node.java
@@ -0,0 +1,10 @@
+//Class untuk menyimpan objek Kendaraan
+public class Node {
+    Kendaraan data;
+    Node next;
+
+    Node(Kendaraan data) {
+        this.data = data;
+        this.next = null;
+    }
+}
